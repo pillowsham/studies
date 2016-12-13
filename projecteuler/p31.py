f469a99d404e3b3225c48900f6829930ac812135
pillowsham <pillowsham@tutanota.com>
2016-12-12 22:12:29 -0500

    Small refactoring to extract a more general-purpose function

--- projecteuler/p31.py
+++ projecteuler/p31.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-SIZE = 200
+def p31(s, coins):
+    arr = [1] + [0]*s
 
-arr = [1] + [0]*SIZE
+    for c in coins:
+        for i in range(c, s+1):
+            arr[i] += arr[i-c]
 
-for c in [1, 2, 5, 10, 20, 50, 100, 200]:
-    for i in range(c, SIZE+1):
-        arr[i] += arr[i-c]
+    return arr[-1]
 
-print(arr[-1])
+print(p31(200, [1, 2, 5, 10, 20, 50, 100, 200]))
